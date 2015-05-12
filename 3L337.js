2be38834156c1c43e3c0a2cc2f47a6df12c58442
Ben Collier <bmcollier@gmail.com>
2015-05-12 13:38:44 +0100

    Added torpedoes.

--- 3L337.js
+++ 3L337.js
@@ -24,12 +24,29 @@ function Ship (x,y,z,name) {
     return ship;
 }
 
+function Torpedo (x,y,z,dx,dy,dz) {
+    var entity = new Entity(x,y,z)
+    var torpedo = Object.create(entity)
+	var geometry = new THREE.SphereGeometry(1,0,0);
+	var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+	torpedo.sprite = new THREE.Mesh( geometry, material );
+	torpedo.sprite.position.x = torpedo.x;
+	torpedo.sprite.position.y = torpedo.y;
+	torpedo.sprite.position.z = torpedo.z;
+	torpedo.dx = dx;
+	torpedo.dy = dy;
+	torpedo.dz = dz;
+	console.log("New torpedo created at " + torpedo.x + ", " + torpedo.y + ", " + torpedo.z);
+    return torpedo
+    }
+
 function render() {
 	requestAnimationFrame(render);
-	for (a=0; a<50; a++) {
-		ship[a].sprite.position.z += 0.01;
-		ship[a].sprite.rotation.x += 0.01;
+	for (i=0; i<ships.length; i++) {
+		ships[i].sprite.position.z += 0.01;
+		ships[i].sprite.rotation.x += 0.01;
 	}
+	torpedo[0].sprite.position.z += torpedo[0].dz
 	renderer.render(scene, camera);
 }
 
@@ -43,12 +60,19 @@ document.body.appendChild( renderer.domElement );
 
 // Instantiate spacecraft
 
-var ship = [];
-for (a=0; a<50; a++) {
-	ship[a] = new Ship(Math.floor(Math.random()*5),Math.floor(Math.random()*5),Math.floor(Math.random()*5),"Daedalus");
-	scene.add(ship[a].sprite);
-	console.log(ship[a].name);
+var ships = [];
+for (a=0; a<2; a++) {
+	ships[a] = new Ship(Math.floor(Math.random()*5),Math.floor(Math.random()*5),Math.floor(Math.random()*5),"Daedalus");
+	scene.add(ships[a].sprite);
+	console.log(ships[a].name);
 }
+
+// Instantiate torpedo
+
+var torpedo = []
+torpedo[0] = new Torpedo(0,0,0,0,0,-1)
+scene.add(torpedo[0].sprite)
+
 //var ship = new Ship(1,1,1,"Daedalus");
 
 camera.position.z = 5;
